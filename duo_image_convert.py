52bfa2ff5da5968d8ccdcbb2d9d9c867e386b177
siffland <siffland@shockwave.insaneplanet.us>
2023-12-27 16:27:08 -0600

    Re-worked script checks datfile to match image

--- duo_image_convert.py
+++ duo_image_convert.py
@@ -7,86 +7,110 @@ import xml.etree.ElementTree as ET
 from PIL import Image
 import struct
 import concurrent.futures
+import re
 
+special_chars = r'[&\/:*`<>?\\"|]'
 target_height = 165
 
-def get_crc_name(filename: str, games: list[ET.ElementTree]) -> Optional[str]:
-    for game_title in games:
-        if game_title.attrib['name'] in filename:
-            rom = game_title.find('rom')
-            crc = rom.attrib.get('crc')
-            if crc:
-                return crc + '.bin'
+def fix_title(title):
+    return re.sub(special_chars, '_', title)
+
+def get_crc_from_game_hucard(game_name: str, games: list[ET.Element]) -> str:
+    normalized_game_name = fix_title(game_name)
+    for game in games:
+        title = game.attrib.get('name')
+        if title and fix_title(title) == normalized_game_name:
+            rom = game.find('rom')
+            if rom is not None:
+                return rom.attrib.get('crc')
     return None
 
-def convert_image(in_path: Path, out_dir: Path, games: list[ET.ElementTree]):
+def get_crc_from_game_cd(game_name: str, games: list[ET.Element]) -> str:
+    normalized_game_name = fix_title(game_name)
+    track_pattern = re.compile(r'\(Track 0?2\)')  # Regex pattern to match both (Track 02) and (Track 2) since both are used
 
-    filename = in_path.name
-    crc_name = get_crc_name(filename, games)
+    for game in games:
+        title = game.attrib.get('name')
+        if title and fix_title(title) == normalized_game_name:
+            bin_files = [rom for rom in game.findall('rom') if rom.attrib.get('name').endswith('.bin')]
 
-    if not crc_name:
-        print(f"No CRC found for {filename}") 
+            # If there's only one .bin file, return its CRC
+            if len(bin_files) == 1:
+                return bin_files[0].attrib.get('crc')
+
+            # If there are multiple .bin files, look for "Track 02"
+            for bin_file in bin_files:
+                if track_pattern.search(bin_file.attrib.get('name')):
+                    return bin_file.attrib.get('crc')
+
+    return None
+
+def find_matching_image(game_name: str, image_dir: Path) -> Optional[Path]:
+    normalized_game_name = fix_title(game_name)
+    for image_path in image_dir.iterdir():
+        if image_path.is_file():
+            image_file_name = fix_title(image_path.stem)
+            if normalized_game_name in image_file_name:
+                return image_path
+    return None
+
+def convert_image(game_name: str, image_dir: Path, out_dir: Path, games: list[ET.Element], is_cd: bool):
+    image_path = find_matching_image(game_name, image_dir)
+    if not image_path:
+        print(f"No matching image found for {game_name}")
         return
 
-    out_path = out_dir / crc_name 
+    crc = get_crc_from_game_cd(game_name, games) if is_cd else get_crc_from_game_hucard(game_name, games)
+    if not crc:
+        print(f"No CRC found for {game_name}")
+        return
 
-    print(f"Converting {filename} to {crc_name}")
-    
-    in_img = Image.open(in_path)  
+    out_path = out_dir / f"{crc}.bin"
+    print(f"Converting {image_path.name} to {out_path.name}")
 
-    width, height = in_img.size  
+    in_img = Image.open(image_path)
+    width, height = in_img.size
     scale = target_height / height
-    in_img = in_img.resize((int(width*scale), target_height))
-
-    rgb_img = in_img.convert("RGBA") 
-
+    in_img = in_img.resize((int(width * scale), target_height))
+    rgb_img = in_img.convert("RGBA")
     pixels = rgb_img.getdata()
 
-    bpp = 4  
+    bpp = 4
     width, height = in_img.size
 
-    image = []
-    image.append(bpp*8)   
-    image.append(0x49)
-    image.append(0x50)
-    image.append(0x41)
+    image = [bpp * 8, 0x49, 0x50, 0x41,
+             width & 0xFF, (width >> 8) & 0xFF,
+             height & 0xFF, (height >> 8) & 0xFF]
 
-    image.append(width&0xFF)
-    image.append((width>>8)&0xFF)    
-    image.append(height&0xFF)
-    image.append((height>>8)&0xFF)
-    
-    for i in range(width*height):
-        image.append(pixels[i][2]) 
-        image.append(pixels[i][1])
-        image.append(pixels[i][0])
-        image.append(pixels[i][3])
+    for i in range(width * height):
+        image.extend([pixels[i][2], pixels[i][1], pixels[i][0], pixels[i][3]])
 
     with open(out_path, 'wb') as f:
-        for i in range((width*height*bpp)+8):
-            f.write(struct.pack('B', image[i]))
-            
-def convert_image_thread(in_path, out_dir, games): 
-    convert_image(in_path, out_dir, games)
+        for byte in image:
+            f.write(struct.pack('B', byte))
+
+def convert_image_thread(game_name, image_dir, out_dir, games, is_cd):
+    convert_image(game_name, image_dir, out_dir, games, is_cd)
 
 def main():
-
     if len(sys.argv) != 4:
         print("Usage: script.py <data_file.dat> <input_dir> <output_dir>")
         sys.exit(1)
 
-    data_file = Path(sys.argv[1])  
-    in_dir = Path(sys.argv[2])
+    data_file = Path(sys.argv[1])
+    image_dir = Path(sys.argv[2])
     out_dir = Path(sys.argv[3])
 
     root = ET.parse(data_file).getroot()
-    games = root.findall('game')  
+    is_cd = 'CD' in root.find('header/name').text	#This checks the header name for the letters CD to determine which function to use
+    games = root.findall('game')
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
         futures = []
-        for in_path in in_dir.iterdir():
-            if in_path.is_file():
-                futures.append(executor.submit(convert_image_thread, in_path, out_dir, games))
+        for game in games:
+            game_name = game.attrib.get('name')
+            if game_name:
+                futures.append(executor.submit(convert_image_thread, game_name, image_dir, out_dir, games, is_cd))
 
         for future in concurrent.futures.as_completed(futures):
             future.result()
